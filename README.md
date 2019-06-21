0024df846ca97ce90557bc1b69572600fb3c2488
Josemar <josemarsilva-inmetrics@bitbucket.org>
2019-06-21 20:01:43 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,34 @@
+# README - devops-labs
+
+## 1. Introdução
+
+### 1.1. Objetivo
+O objetivo deste projeto é compilar, organizar e documentar cenários de laboratórios, práticas e experiências de **DevOps**.
+
+
+### 1.2. O que é DevOps
+O DevOps é um conjunto de práticas que automatizam os processos entre equipes de desenvolvimento de software e de TI para que possam criar, testar e liberar softwares de maneira mais rápida e confiável.
+
+
+### 1.3. Como os cenários estão organizados
+A estratégia do projeto **devops-labs** é agrupar os cenários por critérios crescente de complexidade, dentro de um grupo de assuntos. Será comum encontrar um cenário que referencie um cenário anteriormente descrito, extendendo-o para uma nova funcionalidade. 
+
+Os cenários serão organizados em tópicos comuns, com objetivo de dar uniformidade a leitura e referência. Na parte inicial de cada cenário, terá um __Mapa Mental (Mind Map)__ derivado deste abaixo.
+
+![MindMap DevOps](doc/MindMap%20DevOps.png)
+
+Os cenários poderão conter diagramas de contexto. Serão usados os diagramas UML de __Use Case Diagram__, __Deploy Diagram__, __Activity Diagram__ ,  __BMPN Diagrams__ e qualquer outro tipo de figuras e vídeos com objetivo de enriquecer a documentação do cenário.
+
+
+---
+## 2. Estudo de Caso
+
+### 2.1. Cenários
+
+
+
+---
+## I - Referências
+
+* [Learning Become a Devops Engineer](https://www.linkedin.com/learning/paths/become-a-devops-engineer)
+* [Flow Diagram of Tools used in Devops](https://medium.com/devops-process-and-tools/flow-diagram-of-tools-used-in-devops-b8d9f944ef21)
---
+++ doc/MindMap DevOps.mm
@@ -0,0 +1,146 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1560511944302" ID="ID_14535916" MODIFIED="1561157500300">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      <font size="6"><b>DevOps </b></font>
+    </p>
+    <p style="text-align: center">
+      <font size="4"><i>Mind Map</i></font>
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="gohome"/>
+<node CREATED="1560511984776" ID="ID_1173631083" MODIFIED="1561075653255" POSITION="left">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="4">Continuous </font>
+    </p>
+    <p>
+      <font size="4">Development</font>
+    </p>
+  </body>
+</html></richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="folder"/>
+<node CREATED="1560512150274" ID="ID_508662412" MODIFIED="1561156902295" TEXT="Plan">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1560512156115" ID="ID_718213434" MODIFIED="1561156904900" TEXT="Code">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1560512013898" ID="ID_1498968330" MODIFIED="1561075653255" POSITION="left">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="4">Continuous </font>
+    </p>
+    <p>
+      <font size="4">Testing</font>
+    </p>
+  </body>
+</html></richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="folder"/>
+<node CREATED="1560512177181" ID="ID_931000248" MODIFIED="1561156911908" TEXT="Build">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1560512179567" ID="ID_982319677" MODIFIED="1561156914241" TEXT="Test">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1561156710848" ID="ID_1071559844" MODIFIED="1561157148994" POSITION="left">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font size="4">Tools &amp; Stack</font></b>
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="list"/>
+</node>
+<node CREATED="1560512025839" ID="ID_672184253" MODIFIED="1561075653255" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="4">Continuous </font>
+    </p>
+    <p>
+      <font size="4">Deployment</font>
+    </p>
+  </body>
+</html></richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="folder"/>
+<node CREATED="1560512185562" ID="ID_579638188" MODIFIED="1560539480831" TEXT="Release">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1560512187847" ID="ID_1244701246" MODIFIED="1560539480832" TEXT="Deploy">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1560512039698" ID="ID_1105506534" MODIFIED="1561075653255" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="4">Continuous </font>
+    </p>
+    <p>
+      <font size="4">Monitoring</font>
+    </p>
+  </body>
+</html></richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="folder"/>
+<node CREATED="1560512194023" ID="ID_1743855136" MODIFIED="1560539580773" TEXT="Operate">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1560512197120" ID="ID_1998879544" MODIFIED="1560539580772" TEXT="Monitoring">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1561156710848" ID="ID_1643212578" MODIFIED="1561156838284" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font size="4">Culture</font></b>
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="list"/>
+<node CREATED="1561156922814" ID="ID_284583337" MODIFIED="1561157555744" TEXT="Education">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1561156922814" ID="ID_423511767" MODIFIED="1561157369779" TEXT="Politics">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+</map>
