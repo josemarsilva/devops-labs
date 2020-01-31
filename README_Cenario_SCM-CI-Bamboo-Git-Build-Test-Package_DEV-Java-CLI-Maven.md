59f690145ae54bd18626d488daf0ed88dba7822d
josemarsilva <josemarsilva@yahoo.com.br>
2020-01-31 11:47:17 -0300

    .

---
+++ README_Cenario_SCM-CI-Bamboo-Git-Build-Test-Package_DEV-Java-CLI-Maven.md
@@ -0,0 +1,90 @@
+# README - devops-labs - Cenario_SCM-CI-Bamboo-Git-Build-Test-Package_DEV-Java-CLI-Maven
+
+## 1. Introdução
+
+### 1.1. Objetivo
+O objetivo deste cenário é demonstrar a ferramenta **Bamboo** integrando com o **Github** para buscar o código fonte de um aplicativo construído em linguagem **Java** e **Apache-CLI**. Em seguida o **Bamboo** faz o **Build** com base no **Maven**, executa os testes unitários **Unit Test** já previstas na programação e gera o pacote de aplicativo.
+
+### 1.2. MindMap
+![MindMap DevOps SCM-CI-Bamboo-Git-Build-Test-Package_DEV-Java-Cli-Maven.png](doc/MindMap%20DevOps%20SCM-CI-Bamboo-Git-Build-Test-Package_DEV-Java-Cli-Maven.png)
+
+
+### 1.3. Tópicos abordados
+Este cenário de laboratório aborda os seguintes tópicos, conceitos, práticas e ferramentas:
+* SCM - Source Code GitHub.com
+* CI - Continuos Integration ( Checkout Source Code, Compile, Build, Package )
+* Bamboo - CI Tools
+* Bamboo Project, Plan e Build executando `git pull`
+* Bamboo Project, Plan e Build executando  fazendo o `Build` da aplicação com o `Maven`
+* Automação do processo de `Build`, `UnitTest` e `Package`
+* Cultura do compromisso de ter o código fonte em um repositório (GitHub)
+
+---
+## 2. Cenário
+
+### 2.1. Diagramas 
+
+### a. Use Case Diagram
+
+* Diagrama de Contexto do laboratório
+
+![UseCaseDiagram-Cenario_CI-Bamboo-Git-Build-Test-Package_DEV-Java-SpringBoot-Maven.png](doc/UseCaseDiagram-Cenario_CI-Bamboo-Git-Build-Test-Package_DEV-Java-SpringBoot-Maven.png)
+
+* Diagrama de Contexto da aplicação `java-simplecalc-spring-boot`
+A aplicação [`java-simplecalc-spring-boot` Calculadora Simples de expressão pela web](https://github.com/josemarsilva/java-simplecalc-spring-boot) recebe como parâmetro uma expressão, avalia e apresenta o seu resultado.
+
+![java-simplecalc-spring-boot-UseCaseDiagram-Context.png](https://github.com/josemarsilva/java-simplecalc-spring-boot/blob/master/doc/UseCaseDiagram-Context.png)
+
+
+### b. Deploy Diagram
+![DeployDiagram-Cenario_CI-Bamboo-Git-Build-Test-Package_DEV-Java-SpringBoot-Maven.png](doc/DeployDiagram-Cenario_CI-Bamboo-Git-Build-Test-Package_DEV-Java-SpringBoot-Maven.png)
+
+
+
+### c. BPMN
+![BPMN-Cenario_CI-Bamboo-Git-Build-Test-Package_DEV-Java-SpringBoot-Maven.png](doc/BPMN-Cenario_CI-Bamboo-Git-Build-Test-Package_DEV-Java-SpringBoot-Maven.png)
+
+
+---
+### 2.2. Pré requisitos
+
+* [Bamboo instalado](https://github.com/josemarsilva/eval-virtualbox-vm-ubuntu-server/doc/README_InstallBambooLicense_StepByStep.md)
+* [Guia de Instalação Bamboo Server para Windows](README-GuiaInstalacao-Bambo-Windows.md)
+* Maven 3.6.x instaldo
+
+
+---
+### 2.3. Leitura pré-execução
+
+* não há
+
+---
+### 2.4. Guia de Configuração
+O objetivo é você criar um **projeto** (__project__), **plano** (__plan__) e **construção** (__build__) para realizar o download do código do GitHub e fazer o `Build` e o `Package`.
+
+* Passo 1: Efetue o login no Bamboo Server
+
+![bamboo-printscreen-javasimplecalccli-01-login.png](doc/bamboo-printscreen-javasimplecalccli-01-login.png)
+
+
+
+---
+### 2.5. Guia de Demonstração
+
+* under-construction
+
+
+---
+## 3. Conclusão
+* Observe que neste cenário o Bamboo fez as atividades de "Source Code Management", "Build", "Unit Test", de forma automática
+* Observe que configuramos o __goals__ `clean compile pacakge` no Maven e ele gerou o ".jar" da aplicação, porém ainda não fizemos o "Deploy" desta aplicação 
+
+---
+## I - Referências
+
+* [`java-simplecalc-spring-boot` Calculadora Simples de expressão pela web](https://github.com/josemarsilva/java-simplecalc-spring-boot)
+* [Site Atlassian sobre Bamboo](https://br.atlassian.com/software/bamboo)
+* [Bitbucket Pipeline Build and Deploy](https://bitbucket.org/product/features/pipelines)
+* [Vídeo Youtub - Getting Started with Atlassian Bamboo - Continuous Delivery in Action](https://www.youtube.com/watch?v=rG-XxVYNS4c)
+* [Bamboo vs Jenkins: Which CI/CD tool to use?](https://blog.valiantys.com/en/dev-tools-en/jenkins-vs-bamboo/)
+* [Bamboo vs Jenkins](https://www.automation-consultants.com/bamboo-vs-jenkins/)
---
+++ doc/MindMap DevOps SCM-CI-Bamboo-Git-Build-Test-Package_DEV-Java-CLI-Maven.mm
@@ -0,0 +1,451 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1560511944302" ID="ID_14535916" MODIFIED="1561307849285">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      <font size="6"><b>DevOps </b></font>
+    </p>
+    <p style="text-align: center">
+      <font size="4"><i>Mind Map</i></font>
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="gohome"/>
+<node CREATED="1560511984776" ID="ID_1173631083" MODIFIED="1561305753798" POSITION="left">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="4">Continuous </font>
+    </p>
+    <p>
+      <font size="4">Development</font>
+    </p>
+  </body>
+</html></richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="folder"/>
+<node CREATED="1560512150274" FOLDED="true" ID="ID_508662412" MODIFIED="1561419633960" TEXT="Plan">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="closed"/>
+<node CREATED="1561258133619" ID="ID_437460015" MODIFIED="1561258187573">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Requirement
+    </p>
+    <p>
+      Management
+    </p>
+  </body>
+</html></richcontent>
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1560512156115" ID="ID_718213434" MODIFIED="1561327780318" TEXT="Code">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1561256637139" ID="ID_1263394487" MODIFIED="1561309719818">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Source Code
+    </p>
+    <p>
+      Management
+    </p>
+  </body>
+</html></richcontent>
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1561306192435" ID="ID_759126050" MODIFIED="1561309675088" TEXT="Git">
+<font NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1561256876461" ID="ID_986045375" MODIFIED="1561904328193" TEXT="Code Review">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="closed"/>
+</node>
+<node CREATED="1561305933484" ID="ID_876638900" MODIFIED="1561306098267">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Programming language,
+    </p>
+    <p>
+      framework, etc
+    </p>
+  </body>
+</html></richcontent>
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1561306068484" ID="ID_1103171240" MODIFIED="1561331104089" TEXT="Java">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1561330241668" ID="ID_829075253" MODIFIED="1561419530463">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Spring-Boot
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1561329955249" ID="ID_79307702" MODIFIED="1561329960657" TEXT="Maven">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1561330196596" ID="ID_319336879" MODIFIED="1561330201085" TEXT="JUnit">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1560512013898" ID="ID_1498968330" MODIFIED="1561330027525" POSITION="left">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="4">Continuous </font>
+    </p>
+    <p>
+      <font size="4">Testing</font>
+    </p>
+  </body>
+</html></richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="folder"/>
+<node CREATED="1560512177181" ID="ID_931000248" MODIFIED="1561330036478" TEXT="Build">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1560512179567" ID="ID_982319677" MODIFIED="1561330045464" TEXT="Test">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1561256035460" ID="ID_1328115742" MODIFIED="1561330795181" TEXT="Unit Test">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1561256589600" ID="ID_1850640344" MODIFIED="1561904551169" TEXT="Integration Test">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="closed"/>
+</node>
+<node CREATED="1561258283604" ID="ID_1093836150" MODIFIED="1561330104894" TEXT="Stress Test">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="closed"/>
+</node>
+</node>
+</node>
+<node CREATED="1560512039698" FOLDED="true" ID="ID_1105506534" MODIFIED="1561308289535" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="4">Continuous </font>
+    </p>
+    <p>
+      <font size="4">Monitoring</font>
+    </p>
+  </body>
+</html></richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="folder"/>
+<icon BUILTIN="closed"/>
+<node CREATED="1560512194023" ID="ID_1743855136" MODIFIED="1561306807886" TEXT="Operate">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1560512197120" ID="ID_1998879544" MODIFIED="1561306809198" TEXT="Monitoring">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+<node CREATED="1561156710848" ID="ID_1071559844" MODIFIED="1561306868381" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font size="4">Continuous </font></b>
+    </p>
+    <p>
+      <b><font size="4">Integration</font></b>
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="folder"/>
+<node CREATED="1561257634907" ID="ID_568970222" MODIFIED="1561307784175">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Integrated
+    </p>
+    <p>
+      Repository
+    </p>
+  </body>
+</html></richcontent>
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1561306436907" ID="ID_660299695" MODIFIED="1561309719818">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Source Code
+    </p>
+    <p>
+      Management
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1561308414063" ID="ID_1058993284" MODIFIED="1561309672328" TEXT="Git">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1561309733786" ID="ID_1126951727" MODIFIED="1561309738863" TEXT="Github">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+</node>
+</node>
+<node CREATED="1561257647764" ID="ID_1771238579" MODIFIED="1561330073279" TEXT="Automated Tests">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1561330080093" ID="ID_1620717679" MODIFIED="1561330085245" TEXT="JUnit">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+<node CREATED="1561258037747" ID="ID_1932818956" MODIFIED="1561308017911">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Automated Pipeline
+    </p>
+    <p>
+      execution
+    </p>
+  </body>
+</html></richcontent>
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1561308089198" ID="ID_368099354" MODIFIED="1561419591492" TEXT="Jenkins">
+<icon BUILTIN="closed"/>
+</node>
+<node CREATED="1561419570822" ID="ID_429311992" MODIFIED="1561419576136" TEXT="Bamboo">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1561331035575" ID="ID_156298585" MODIFIED="1561331039921" TEXT="Maven">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+</node>
+<node CREATED="1561156710848" ID="ID_1643212578" MODIFIED="1561305753817" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font size="4">Culture</font></b>
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="folder"/>
+<node CREATED="1561156922814" ID="ID_284583337" MODIFIED="1561306886165" TEXT="Education">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="closed"/>
+</node>
+<node CREATED="1561156922814" ID="ID_423511767" MODIFIED="1561306674836" TEXT="Commitment">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="button_ok"/>
+<node CREATED="1561306436907" ID="ID_1587091865" MODIFIED="1561308967072">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Source Code
+    </p>
+    <p>
+      Management
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1561329983634" ID="ID_1788508811" MODIFIED="1561330006335">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Maven Project
+    </p>
+    <p>
+      (pom.xml)
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1561330872838" ID="ID_132058984" MODIFIED="1561330943473">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Unit Test by
+    </p>
+    <p>
+      developer
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+</node>
+<node CREATED="1560512025839" FOLDED="true" ID="ID_672184253" MODIFIED="1561330427197" POSITION="left">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <font size="4">Continuous </font>
+    </p>
+    <p>
+      <font size="4">Deployment</font>
+    </p>
+  </body>
+</html></richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="folder"/>
+<icon BUILTIN="closed"/>
+<node CREATED="1560512185562" ID="ID_579638188" MODIFIED="1561306799942" TEXT="Release">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1560512187847" FOLDED="true" ID="ID_1244701246" MODIFIED="1561306800391" TEXT="Deploy">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1561256090380" ID="ID_1574451859" MODIFIED="1561258721419">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Quality
+    </p>
+    <p>
+      Assurance
+    </p>
+  </body>
+</html></richcontent>
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1561256123376" ID="ID_1850186943" MODIFIED="1561256603609" TEXT="Production">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+</node>
+</node>
+</node>
+<node CREATED="1561156710848" ID="ID_417306774" MODIFIED="1561305893977" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><font size="4">Tools</font></b>
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="folder"/>
+<node CREATED="1561213490096" ID="ID_112888288" MODIFIED="1561309107856" TEXT="Stack">
+<font ITALIC="true" NAME="SansSerif" SIZE="12"/>
+<node CREATED="1561306224803" ID="ID_1261003126" MODIFIED="1561306229733" TEXT="DEV">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1561306405661" ID="ID_759937739" MODIFIED="1561331104089" TEXT="Java">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1561330241668" ID="ID_396886730" MODIFIED="1561419546349">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Spring-Boot
+    </p>
+  </body>
+</html></richcontent>
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1561329955249" ID="ID_72154994" MODIFIED="1561329960657" TEXT="Maven">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1561330196596" ID="ID_807839473" MODIFIED="1561330201085" TEXT="JUnit">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+</node>
+<node CREATED="1561306459728" ID="ID_547342853" MODIFIED="1561306894509" TEXT="CT">
+<icon BUILTIN="closed"/>
+</node>
+<node CREATED="1561306471699" ID="ID_976994284" MODIFIED="1561306896220" TEXT="CD">
+<icon BUILTIN="closed"/>
+</node>
+<node CREATED="1561306479969" ID="ID_657077696" MODIFIED="1561306896707" TEXT="CM">
+<icon BUILTIN="closed"/>
+</node>
+<node CREATED="1561305851017" ID="ID_1823599983" MODIFIED="1561306138340" TEXT="CI">
+<icon BUILTIN="button_ok"/>
+<node CREATED="1561308089198" ID="ID_1929916934" MODIFIED="1561419671227" TEXT="Jenkins">
+<icon BUILTIN="closed"/>
+</node>
+<node CREATED="1561419647011" ID="ID_410228023" MODIFIED="1561419652502" TEXT="Bamboo">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1561308454174" ID="ID_512233864" MODIFIED="1561308475224" TEXT="GitHub">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1561329963996" ID="ID_920567906" MODIFIED="1561329967659" TEXT="Maven">
+<icon BUILTIN="button_ok"/>
+</node>
+<node CREATED="1561330170353" ID="ID_1860306333" MODIFIED="1561330175516" TEXT="JUnit">
+<icon BUILTIN="button_ok"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</map>
