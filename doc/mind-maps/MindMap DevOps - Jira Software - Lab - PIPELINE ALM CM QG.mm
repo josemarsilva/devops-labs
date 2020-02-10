4286f15945c68fc81a848759932856ec2e9425b4
josemarsilva <josemar.furegatti@gmail.com>
2020-02-10 08:15:51 -0300

    .

---
+++ doc/mind-maps/MindMap DevOps - Jira Software - Lab - PIPELINE ALM CM QG.mm
@@ -0,0 +1,360 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1581285463402" ID="ID_599687315" MODIFIED="1581289785154">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p style="text-align: center">
+      <b>Jira Software</b>
+    </p>
+    <p style="text-align: center">
+      DevOps Labs
+    </p>
+  </body>
+</html></richcontent>
+<font NAME="SansSerif" SIZE="13"/>
+<icon BUILTIN="gohome"/>
+<node CREATED="1581286342805" ID="ID_1777749471" MODIFIED="1581289787488" POSITION="left">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b>P</b>ipeline <b>DevOps</b>
+    </p>
+  </body>
+</html>
+</richcontent>
+<font ITALIC="true" NAME="SansSerif" SIZE="13"/>
+<icon BUILTIN="full-1"/>
+<node CREATED="1581286379600" ID="ID_911315274" MODIFIED="1581289789629">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Issue
+    </p>
+    <p>
+      Types
+    </p>
+  </body>
+</html></richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="list"/>
+<node CREATED="1581286421110" ID="ID_211271047" MODIFIED="1581288577374" TEXT="Pacote"/>
+<node CREATED="1581286423847" ID="ID_692824934" MODIFIED="1581289820008">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Item Pacote
+    </p>
+  </body>
+</html>
+</richcontent>
+</node>
+</node>
+<node CREATED="1581286379600" ID="ID_896149253" MODIFIED="1581289789646" TEXT="Workflow">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="launch"/>
+<node CREATED="1581286415701" ID="ID_1380270724" MODIFIED="1581290112422" TEXT="CRIADO"/>
+<node CREATED="1581286465398" ID="ID_1633286460" MODIFIED="1581290114406" TEXT="EM ANDAMENTO"/>
+<node CREATED="1581286472175" ID="ID_1195420263" MODIFIED="1581290125742" TEXT="EM REVIS&#xc3;O"/>
+<node CREATED="1581286603977" ID="ID_1993308816" MODIFIED="1581290128244" TEXT="RELEASE PACKAGED">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1581286608355" ID="ID_1070757966" MODIFIED="1581290135055" TEXT="DEPLOY TU">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1581286613547" ID="ID_201662038" MODIFIED="1581290135055" TEXT="DEPLOY TI">
+<font NAME="SansSerif" SIZE="12"/>
+</node>
+<node CREATED="1581286617785" ID="ID_260629712" MODIFIED="1581290132030" TEXT="DEPLOY PROD"/>
+<node CREATED="1581289037035" ID="ID_1740952486" MODIFIED="1581290116050" TEXT="CANCELADO"/>
+</node>
+<node CREATED="1581286379600" ID="ID_1914987704" MODIFIED="1581289789658">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Custom
+    </p>
+    <p>
+      Fields
+    </p>
+  </body>
+</html></richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="edit"/>
+<node CREATED="1581289190280" ID="ID_56088586" MODIFIED="1581289789782">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Tipo Processo
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="list"/>
+<node CREATED="1581289260973" ID="ID_479924399" MODIFIED="1581289323804" TEXT="Normal"/>
+<node CREATED="1581289263774" ID="ID_1763052995" MODIFIED="1581289323805" TEXT="Emergencial"/>
+<node CREATED="1581289289528" ID="ID_40544493" MODIFIED="1581289323805" TEXT="Equaliza&#xe7;&#xe3;o"/>
+</node>
+<node CREATED="1581289378785" ID="ID_1726143061" MODIFIED="1581289928833">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Info B&#225;sica
+    </p>
+  </body>
+</html>
+</richcontent>
+<icon BUILTIN="edit"/>
+<node CREATED="1581286421110" ID="ID_146219260" MODIFIED="1581287797005" TEXT="C.Custo"/>
+<node CREATED="1581286423847" ID="ID_1651622719" MODIFIED="1581287807951" TEXT="Componente"/>
+<node CREATED="1581288148727" ID="ID_1981739697" MODIFIED="1581289186657">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Changeman
+    </p>
+    <p>
+      Package Id
+    </p>
+  </body>
+</html></richcontent>
+</node>
+</node>
+<node CREATED="1581289622722" ID="ID_998665279" MODIFIED="1581289905269" TEXT="Info Deploy">
+<node CREATED="1581289657473" ID="ID_1385818358" MODIFIED="1581289790081" TEXT="Servidores Produ&#xe7;&#xe3;o">
+<node CREATED="1581289751070" ID="ID_1499340499" MODIFIED="1581289752408" TEXT="AL1"/>
+<node CREATED="1581289753008" ID="ID_747924272" MODIFIED="1581289754146" TEXT="MZ1"/>
+</node>
+<node CREATED="1581289697349" ID="ID_218933312" MODIFIED="1581289790083" TEXT="Ambientes Deploy">
+<node CREATED="1581289705766" ID="ID_1430444736" MODIFIED="1581289708559" TEXT="TU"/>
+<node CREATED="1581289709114" ID="ID_1824871217" MODIFIED="1581289709854" TEXT="TI"/>
+<node CREATED="1581289710093" ID="ID_1383324515" MODIFIED="1581289712294" TEXT="PROD"/>
+</node>
+</node>
+<node CREATED="1581289938157" ID="ID_31945927" MODIFIED="1581289941929" TEXT="Info Aprova&#xe7;&#xe3;o">
+<node CREATED="1581289952302" ID="ID_914528152" MODIFIED="1581289980475" TEXT="RDM (id)"/>
+<node CREATED="1581289996353" ID="ID_1870652386" MODIFIED="1581289999768" TEXT="Aprova&#xe7;&#xe3;o #1"/>
+<node CREATED="1581290000320" ID="ID_794706249" MODIFIED="1581290005231" TEXT="Aprova&#xe7;&#xe3;o #2"/>
+</node>
+</node>
+</node>
+<node CREATED="1581286342805" FOLDED="true" ID="ID_613407206" MODIFIED="1581289784231" POSITION="left">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><u>Q</u></b>uality <b><u>G</u></b>ates
+    </p>
+  </body>
+</html>
+</richcontent>
+<font ITALIC="true" NAME="SansSerif" SIZE="13"/>
+<icon BUILTIN="full-2"/>
+<node CREATED="1581286379600" FOLDED="true" ID="ID_1857087626" MODIFIED="1581289784018">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Issue
+    </p>
+    <p>
+      Types
+    </p>
+  </body>
+</html></richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="list"/>
+<node CREATED="1581286421110" ID="ID_1036029847" MODIFIED="1581286423482" TEXT="Task"/>
+<node CREATED="1581286423847" ID="ID_624032731" MODIFIED="1581286432065" TEXT="Sub-Task"/>
+</node>
+<node CREATED="1581286379600" FOLDED="true" ID="ID_150343968" MODIFIED="1581289784019" TEXT="Workflow">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="launch"/>
+<node CREATED="1581286415701" ID="ID_1582825138" MODIFIED="1581286585773" TEXT="OPEN"/>
+<node CREATED="1581286465398" ID="ID_1219486830" MODIFIED="1581286468218" TEXT="IN PROGRESS"/>
+<node CREATED="1581286472175" ID="ID_1096010041" MODIFIED="1581286600592" TEXT="UNDER REVIEW"/>
+<node CREATED="1581286603977" ID="ID_1433256123" MODIFIED="1581286605878" TEXT="APPROVED"/>
+<node CREATED="1581286608355" ID="ID_654156604" MODIFIED="1581286610197" TEXT="DONE"/>
+<node CREATED="1581286613547" ID="ID_1219482952" MODIFIED="1581286615443" TEXT="REJECTED"/>
+<node CREATED="1581286617785" ID="ID_1978777903" MODIFIED="1581286625632" TEXT="CANCELLED"/>
+</node>
+<node CREATED="1581286379600" FOLDED="true" ID="ID_1597055499" MODIFIED="1581289784019">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Custom
+    </p>
+    <p>
+      Fields
+    </p>
+  </body>
+</html></richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="edit"/>
+<node CREATED="1581286421110" ID="ID_1113995111" MODIFIED="1581287797005" TEXT="C.Custo"/>
+<node CREATED="1581286423847" ID="ID_122205809" MODIFIED="1581287807951" TEXT="Componente"/>
+</node>
+</node>
+<node CREATED="1581286342805" FOLDED="true" ID="ID_1134575191" MODIFIED="1581289784232" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><u>A</u></b>pplication <b><u>L</u></b>ifecycle
+    </p>
+    <p>
+      <b><u>M</u></b>anagement
+    </p>
+  </body>
+</html>
+</richcontent>
+<font ITALIC="true" NAME="SansSerif" SIZE="13"/>
+<icon BUILTIN="full-3"/>
+<node CREATED="1581286379600" FOLDED="true" ID="ID_189325054" MODIFIED="1581289784020">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Issue
+    </p>
+    <p>
+      Types
+    </p>
+  </body>
+</html></richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="list"/>
+<node CREATED="1581286421110" ID="ID_1757843749" MODIFIED="1581286423482" TEXT="Task"/>
+<node CREATED="1581286423847" ID="ID_1596640123" MODIFIED="1581286432065" TEXT="Sub-Task"/>
+</node>
+<node CREATED="1581286379600" FOLDED="true" ID="ID_334323012" MODIFIED="1581289784020" TEXT="Workflow">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="launch"/>
+<node CREATED="1581286415701" ID="ID_1157675269" MODIFIED="1581286585773" TEXT="OPEN"/>
+<node CREATED="1581286465398" ID="ID_1963005206" MODIFIED="1581286468218" TEXT="IN PROGRESS"/>
+<node CREATED="1581286472175" ID="ID_1527176946" MODIFIED="1581286600592" TEXT="UNDER REVIEW"/>
+<node CREATED="1581286603977" ID="ID_1489841068" MODIFIED="1581286605878" TEXT="APPROVED"/>
+<node CREATED="1581286608355" ID="ID_1129160958" MODIFIED="1581286610197" TEXT="DONE"/>
+<node CREATED="1581286613547" ID="ID_1514811505" MODIFIED="1581286615443" TEXT="REJECTED"/>
+<node CREATED="1581286617785" ID="ID_704430616" MODIFIED="1581286625632" TEXT="CANCELLED"/>
+</node>
+<node CREATED="1581286379600" FOLDED="true" ID="ID_678619874" MODIFIED="1581289784021">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Custom
+    </p>
+    <p>
+      Fields
+    </p>
+  </body>
+</html></richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="edit"/>
+<node CREATED="1581286421110" ID="ID_526442969" MODIFIED="1581287797005" TEXT="C.Custo"/>
+<node CREATED="1581286423847" ID="ID_138172204" MODIFIED="1581287807951" TEXT="Componente"/>
+</node>
+</node>
+<node CREATED="1581286342805" FOLDED="true" ID="ID_647457316" MODIFIED="1581289784232" POSITION="right">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      <b><u>C</u></b>hange <b><u>M</u></b>anagement
+    </p>
+  </body>
+</html>
+</richcontent>
+<font ITALIC="true" NAME="SansSerif" SIZE="13"/>
+<icon BUILTIN="full-4"/>
+<node CREATED="1581286379600" FOLDED="true" ID="ID_783272650" MODIFIED="1581289784021">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Issue
+    </p>
+    <p>
+      Types
+    </p>
+  </body>
+</html></richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="list"/>
+<node CREATED="1581286421110" ID="ID_1163736491" MODIFIED="1581286423482" TEXT="Task"/>
+<node CREATED="1581286423847" ID="ID_580887461" MODIFIED="1581286432065" TEXT="Sub-Task"/>
+</node>
+<node CREATED="1581286379600" FOLDED="true" ID="ID_268591688" MODIFIED="1581289784022" TEXT="Workflow">
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="launch"/>
+<node CREATED="1581286415701" ID="ID_1720852395" MODIFIED="1581286585773" TEXT="OPEN"/>
+<node CREATED="1581286465398" ID="ID_200888334" MODIFIED="1581286468218" TEXT="IN PROGRESS"/>
+<node CREATED="1581286472175" ID="ID_1566517644" MODIFIED="1581286600592" TEXT="UNDER REVIEW"/>
+<node CREATED="1581286603977" ID="ID_1390650267" MODIFIED="1581286605878" TEXT="APPROVED"/>
+<node CREATED="1581286608355" ID="ID_22872600" MODIFIED="1581286610197" TEXT="DONE"/>
+<node CREATED="1581286613547" ID="ID_788345464" MODIFIED="1581286615443" TEXT="REJECTED"/>
+<node CREATED="1581286617785" ID="ID_137404201" MODIFIED="1581286625632" TEXT="CANCELLED"/>
+</node>
+<node CREATED="1581286379600" FOLDED="true" ID="ID_590001734" MODIFIED="1581289784022">
+<richcontent TYPE="NODE"><html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Custom
+    </p>
+    <p>
+      Fields
+    </p>
+  </body>
+</html></richcontent>
+<font BOLD="true" NAME="SansSerif" SIZE="12"/>
+<icon BUILTIN="edit"/>
+<node CREATED="1581286421110" ID="ID_574630261" MODIFIED="1581287797005" TEXT="C.Custo"/>
+<node CREATED="1581286423847" ID="ID_590960639" MODIFIED="1581287807951" TEXT="Componente"/>
+</node>
+</node>
+</node>
+</map>
