4475c707014055af50b6d0fa34c7da348fb3f25d
Josemar Silva <josemarsilva@yahoo.com.br>
2022-08-10 15:12:09 -0300

    feat: Azure DevOps pipeline

---
+++ md/mind-maps/Novo mapa mental.mm
@@ -0,0 +1,4 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1660133600801" ID="ID_588793786" MODIFIED="1660133600801" TEXT="Novo mapa mental"/>
+</map>
